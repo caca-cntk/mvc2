4253b42a8cd735b56d98a9c128d34020ddd3b13d
caca <melisadvpr@gmail.com>
2023-07-23 23:58:47 -0700

    menambahkan fitur detail

--- app/views/murid/index.php
+++ app/views/murid/index.php
@@ -9,7 +9,9 @@
 
 <ul class="list-group">
       <?php foreach( $data['murid'] as $murid) : ?>
-        <li class="list-group-item"><?= $murid['nama']; ?></li>
+        <li class="list-group-item d-flex justify-content-between align-items-center"><?= $murid['nama']; ?>
+        <a href="<?= BASEURL; ?>murid/detail/<?= $murid['id']; ?>" class="badge badge-primary">detail</a>
+      </li>
         <?php endforeach; ?>  
     </ul>
   </div>
